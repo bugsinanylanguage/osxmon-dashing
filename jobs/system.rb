bb57f7bd6c98f210c928d9bf7e5b3f81b86fda7e
bugsinanylanguage <bugsinanylanguage@users.noreply.github.com>
2014-09-07 23:35:30 -0700

    now with working graphs

--- jobs/system.rb
+++ jobs/system.rb
@@ -1,90 +1,140 @@
 require 'socket'
 require 'usagewatch_ext'
+require 'system/getifaddrs'
+
+@interval = 2
+units = 's'
+loopTime = "#{@interval}#{units}"
+graphHistorySize = 10
 
 hostname = "localhost"
 ipaddrs = ["127.0.0.1"]
 details = Hash.new({ value: 0 })
-usw = Usagewatch
-pointsHash = Hash.new({ value: 0 })
-
-startArray = Array.new(10){ Hash.new({ y: 0, x: 0}) }
-
-stats = Hash.new({
-  duGig: startArray,
-  duPct: startArray,
-  cpu: startArray,
-  memPct: startArray,
-  sysload: startArray,
-  rxMb: startArray,
-  txMb: startArray
-})
 
 cpuProc = Hash.new({ value: 0 })
 memProc = Hash.new({ value: 0 })
 lastcpuProc = Hash.new({ value: 0 })
 lastmemProc = Hash.new({ value: 0 })
 
-def get_new_stat(statname)
-  value = 0
-  case statname
-    when 'duGig'
-      value = usw.uw_diskused
-    when 'duPct'
-      value = usw.uw_diskused_perc
-    when 'cpu'
-      value = usw.uw_cpuused
-    when 'memPct'
-      value = usw.uw_memused
-    when 'sysload'
-      value = usw.uw_load
-    when 'rxMb'
-      value = usw.uw_bandrx
-    when 'txMb'
-      value = usw.uw_bandtx
+class GraphPoints
+
+  def initialize( arraySize )
+    @max = arraySize
+    @pointsArray = Array.new(arraySize)
+    @pointsArray.push( { :x => 0, :y => 0 })
   end
-  return value
+
+  def getLastX()
+    point = @pointsArray.last
+    return point[ :x ]
+  end
+
+  def getLastY()
+    point = @pointsArray.last
+    return point[ :y ]
+  end
+
+  def update( x, y )
+    point = { :x => x, :y => y }
+    if @pointsArray.count > @max
+      @pointsArray.shift
+    end
+    @pointsArray.push( point )
+  end
+
+  def getPoints()
+    return @pointsArray
+  end
+
 end
 
-SCHEDULER.every '2s', :first_in => 0 do |job|
+@stats = {
+  :duGig => GraphPoints.new(graphHistorySize),
+  :duPct => GraphPoints.new(graphHistorySize),
+  :cpu => GraphPoints.new(graphHistorySize),
+  :memPct => GraphPoints.new(graphHistorySize),
+  :sysload => GraphPoints.new(graphHistorySize),
+  :rxMb => GraphPoints.new(graphHistorySize),
+  :txMb => GraphPoints.new(graphHistorySize)
+}
+
+def self.updateStats
+  @stats.each do | stat, stuff |
+    newX = stuff.getLastX + @interval
+    newY = getNewStat( "#{stat}" )
+    stuff.update( newX, newY )
+  end
+end
+
+def self.bandrx( netiface )
+  read1 =`netstat -ib | grep -e "#{netiface}" -m 1 | awk '{print $7}'`
+  sleep 1
+  read2=`netstat -ib | grep -e "#{netiface}" -m 1 | awk '{print $7}'`
+  (((read2.to_f - read1.to_f)/1024)/1024).round(3)
+end
+
+def self.bandtx( netiface )
+  send1=`netstat -ib | grep -e "#{netiface}" -m 1 | awk '{print $10}'`
+  sleep 1
+  send2=`netstat -ib | grep -e "#{netiface}" -m 1 | awk '{print $10}'`
+  (((send2.to_f - send1.to_f)/1024)/1024).round(3)
+end
+
+def self.getNewStat(statname)
+  case statname
+    when 'duGig'
+      return Usagewatch.uw_diskused
+    when 'duPct'
+      return Usagewatch.uw_diskused_perc
+    when 'cpu'
+      return Usagewatch.uw_cpuused
+    when 'memPct'
+      return Usagewatch.uw_memused
+    when 'sysload'
+      return Usagewatch.uw_load
+    when 'rxMb'
+      return bandrx('en0')
+    when 'txMb'
+      return bandtx('en0')
+  end
+  puts "Default value returned"
+  return 0
+end
+
+SCHEDULER.every loopTime, :first_in => 0 do |job|
   hostname = Socket.gethostname
-  ipaddrs = Socket.ip_address_list
+  ipaddrs = System.get_all_ifaddrs
+
   details['name'] = {
     label: 'hostname',
     value: hostname
   }
-  ipaddrs.each do |ip|
-    ipstring = ip.ip_address
-    details[ipstring] = {
-      label: 'local ip',
-      value: ipstring
+
+  ipaddrs.each do | net |
+    details[ net[ :interface ] ] = {
+      label: net[ :interface ],
+      value: net[ :inet_addr ]
     }
   end
 
-  #graph-style data
-  stats.each do | stat, stuff |
-    stuff.shift
-    stuff[:x] = stuff[:y]
-    stuff[:y] = get_new_stat(stat)
-    stuff << { x: stuff[:x], y: stuff[:y] }
-    stats[stat] = stuff
-    send_event( stat + "_graph", points: stuff )
+  updateStats
+  @stats.each do | stat, stuff |
+    send_event( "#{stat}Graph", points: stuff.getPoints() )
   end
 
   send_event('hostdetaildata', { items: details.values })
-  pointsHash.each do |stat|
-    send_event( stat + "_gauge", value: pointsHash[stat] )
-  end
+
   lastcpuProc = cpuProc
   lastmemProc = memProc
   cpuProc = Hash.new({ value: 0})
   memProc = Hash.new({ value: 0})
-  usw.uw_cputop.each do |cproc|
+  Usagewatch.uw_cputop.each do |cproc|
     cpuProc[cproc[0]] = {
       label: cproc[0],
       value: cproc[1]
     }
   end
-  usw.uw_memtop.each do |mproc|
+  Usagewatch.uw_memtop.each do |mproc|
     memProc[mproc[0]] = {
       label: mproc[0],
       value: mproc[1]
